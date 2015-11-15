dbd13758360025ee4107953bd5eca36c1bb15518
JeffRuLz <yomanrulz@hotmail.com>
2015-11-14 22:08:39 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,6 +11,7 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -19,8 +21,10 @@ public class Main {
 	private static Window window = new Window();
 	private static String localPath = new File(".").getAbsolutePath();
 	private static String gfxFileLocation = "";
-	private static String lastSave = "";
+	private static String lastExport = "";
+	private static String lastImport = "";
 	private static ArrayList<Integer> sheetIndex = new ArrayList<Integer>();
+	private static ArrayList<Integer> sheetSize = new ArrayList<Integer>();
 	
 	public static void main(String[] args) throws IOException {
 		//Load file paths
@@ -41,58 +45,61 @@ public class Main {
 		Window.btnImport.setEnabled(enable);
 	}
 		
-	//The method performs all tasks  that occur when a .qda file is selected
+	//The method performs all tasks that occur when a .qda file is selected
 	private static boolean loadGfxFile() throws IOException {
 		boolean result = true;
-		if (populateFileNames() == true) {
-			populateSheetIndex();
-			updatePreview(Window.cbFileNames.getSelectedIndex());
-		}else{
-			result = false;
+		
+		File f = new File(gfxFileLocation);
+		InputStream is = new FileInputStream(f);		
+		
+		//Get number of sheets
+		is.skip(8);
+		int numOfSheets = is.read();
+		
+		is.skip(247);
+		
+		sheetIndex.clear();
+		sheetSize.clear();
+		
+		Window.clearFileNames();
+		//Get data from sheet headers
+		for (int i = 0; i < numOfSheets; i++) {
+			byte[] buf = new byte[268];
+			is.read(buf);
+			
+			int bmpPos = (buf[0] & 0xff) + ((buf[1] & 0xff) * 256) + ((buf[2] & 0xff) * 65536) + ((buf[3] & 0xff) * 16777216);
+			int bmpSize = (buf[4] & 0xff) + ((buf[5] & 0xff) * 256) + ((buf[6] & 0xff) * 65536) + ((buf[7] & 0xff) * 16777216);
+			
+			String fname = "";
+			for (int a = 0; a < 256; a++) {
+				fname += (char)(buf[12 + a] & 0xff);
+			}
+			
+			//Trim filename			
+			for (int a = 255; a > 0; a--) {
+				if (fname.charAt(a) == 0) {
+					fname = fname.substring(0, a);
+				}else{
+					a = 0;
+				}
+			}
+			
+			//Save data
+			sheetIndex.add(bmpPos);
+			sheetSize.add(bmpSize);
+			Window.cbFileNames.addItem(fname);
+			
+			/*
+			System.out.println(bmpPos);
+			System.out.println(bmpSize);
+			System.out.println(fname);
+			*/
 		}
+
+		is.close();
 		return result;
 	}
-	
-	//Searches the file for sheets and saves their locations
-	//Assumes gfxFileLocation is not empty
-	public static void populateSheetIndex() throws IOException {
-		InputStream is = null;
-		try {
-			is = new FileInputStream(new File(gfxFileLocation));
-			sheetIndex.clear();
-			
-			int read = is.read();
-			int byteCounter = 0;
-			int byte1 = 0, byte2 = 0;
-			
-			while (read != -1) {
-				
-				if (byte1 == (int)'B' && byte2 == (int)'M' && read != (int)'P' && read != (int)'p') {
-					//Found a sheet
-					sheetIndex.add(byteCounter - 2);
-					//System.out.println("" + (byteCounter - 2));
-				}
-				
-				byte1 = byte2;
-				byte2 = read;				
-				read = is.read();
-				
-				byteCounter++;
-			}
-			
-			sheetIndex.add(byteCounter);
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}finally{
-			if (is != null) {
-				try {
-					is.close();
-				} catch (IOException e) {e.printStackTrace();}
-			}
-		}
-	}
-	
+		
 	public static String getFileExtension(String path) {
 		String result = "";
 		
@@ -107,69 +114,95 @@ public class Main {
 		return result;
 	}
 	
-	public static void openQdaFile() throws IOException {
-		File dialogPath = new File(gfxFileLocation);
-		if (gfxFileLocation.equals("")) {
-			dialogPath = new File(localPath);
-		}
-		
+	public static File openFile(String fpath, String description, String extensions) {
+		File dialogPath = new File(fpath);
 		JFileChooser chooser = new JFileChooser(dialogPath);
-		FileFilter filter = new FileNameExtensionFilter("QDA file", "qda");
+		FileFilter filter = new FileNameExtensionFilter(description, extensions);
 		chooser.setFileFilter(filter);
 		
 		int returnVal = chooser.showOpenDialog(chooser);
+		
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			showButtons(false);
-			
-			File gfxFile = chooser.getSelectedFile();
-			System.out.println(getFileExtension(gfxFile.getPath()));
-			
-			if (getFileExtension(gfxFile.getPath()).equals("qda")){				
-				gfxFileLocation = gfxFile.toString();
+			return chooser.getSelectedFile();
+		}
+		
+		return null;
+	}
+	
+	//Quickly return a single byte in a file
+	public static char checkByte(File f, int pos) throws IOException {
+		char result;
+		InputStream is = new FileInputStream(f);
+		
+		is.skip(pos);
+		result = (char)is.read();
+		is.close();
+		
+		return result;
+	}
+	
+	public static void openQdaFile() throws IOException {
+		//Start at last opened QDA file...
+		String startPath = gfxFileLocation;
+		//...otherwise start at local directory
+		File check = new File(gfxFileLocation);
+		if (!check.exists()) {
+			startPath = localPath;
+		}
+		
+		File gfxFile = openFile(startPath, "QDA file", "qda");		
+		if (gfxFile != null) {
+			//Check to see if it is a valid qda file
+			if (checkByte(gfxFile, 4) == 'Q' && checkByte(gfxFile, 5) == 'D' &&
+				checkByte(gfxFile, 6) == 'A' && checkByte(gfxFile, 7) == '0')
+			{
+				gfxFileLocation = gfxFile.toString();				
+				//System.out.println("Suckfest " + (int)checkByte(gfxFile, 8));
+				
+				if (loadGfxFile() == true) {
+					saveConfig();					
+					showButtons(true);
+				}
 			}else{
-				Window.showMessage("Please select a file with a .qda extension.");
+				Window.showMessage("Please select a valid .QDA file.");
 			}
-			
-			if (loadGfxFile() == true) {
-				saveConfig();					
-				showButtons(true);
-			}
-			
-			
 		}
 	}
 	
-	public static void exportImage() {
-		File f = new File(lastSave + "\\" + Window.cbFileNames.getSelectedItem());
+	public static File saveFile(String fpath) {
+		File dialogPath = new File(fpath);
+		
 		JFileChooser chooser = new JFileChooser();
-		chooser.setSelectedFile(f);
+		chooser.setSelectedFile(dialogPath);
 		int returnVal = chooser.showSaveDialog(chooser);
 		
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			showButtons(false);
-			
-			File chosenFile = chooser.getSelectedFile();
-			lastSave = chosenFile.getParentFile().toString();
+			return chooser.getSelectedFile();
+		}
+		
+		return null;
+	}
+	
+	public static void exportImage() {
+		showButtons(false);
+		File f = saveFile(lastExport + "\\" + Window.cbFileNames.getSelectedItem());
+		
+		if (f != null) {
+			lastExport = f.getParentFile().toString();
 			saveConfig();
 			
 			int sheet = Window.cbFileNames.getSelectedIndex();
-			
-			int firstByte = sheetIndex.get(sheet),
-				lastByte = sheetIndex.get(sheet + 1);
-			//int read = 0;
-			
 			InputStream is = null;
 			OutputStream os = null;
-			
 			try {
 				is = new FileInputStream(new File(gfxFileLocation));
-				os = new FileOutputStream(chosenFile);
+				os = new FileOutputStream(f);
+				is.skip(sheetIndex.get(sheet));
 				
-				is.skip(firstByte);
-				for (int i = lastByte - firstByte; i > 0; i--) {
-					os.write(is.read());
-				}
+				byte[] buf = new byte[sheetSize.get(sheet)];
 				
+				is.read(buf);
+				os.write(buf, 0, sheetSize.get(sheet));
 			}catch(Exception e){
 				Window.showMessage("File Export Failed.");
 			}finally{
@@ -179,117 +212,182 @@ public class Main {
 				} catch (IOException e) { e.printStackTrace(); }
 				Window.showMessage("File Export Complete.");
 			}
-			
-			showButtons(true);
-		}		
+		}
+		showButtons(true);	
+		
+	}
+	
+	public static void writeLittleEndian(OutputStream os, long val, int spaces) {
+		long v = val;
+		for (int ii = 0; ii < spaces; ii++) {
+			try {
+				if (ii == 0) {
+					os.write((int)val % (256));
+				}else{
+					int r = (int)Math.floor((int)val / Math.pow(256,ii));
+					//System.out.println(r);
+					os.write(r);
+				}
+			} catch (IOException e) {e.printStackTrace();}
+		}
 	}
 	
 	public static void importImage() throws IOException {
+		showButtons(false);
 		
-		File f = new File(lastSave + "\\" + Window.cbFileNames.getSelectedItem());
-		JFileChooser chooser = new JFileChooser();
-		chooser.setSelectedFile(f);
-		int returnVal = chooser.showOpenDialog(chooser);
+		File f = openFile(lastImport, "BMP File", "bmp");
+		File newFile = new File("temp.qda");
 		
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			
-			showButtons(false);
-			
-			File chosenFile = chooser.getSelectedFile();
-			lastSave = chosenFile.getParentFile().toString();
-			saveConfig();
-			
-			InputStream is = null;
-			OutputStream os = null;
-			
-			try {
-				is = new FileInputStream(gfxFileLocation);
-				ArrayList<Integer> newFile = new ArrayList<Integer>();
+		if (f != null) {
+			//Check to see if the selected file is a BMP file
+			if (checkByte(f, 0) == 'B' && checkByte(f, 1) == 'M') {
+				lastImport = f.getParentFile().toString();
+				System.out.println(lastImport);
+				saveConfig();
+				
+				long byteTracker = 0;
+				int numOfSheets = 0;
+				
 				int sheet = Window.cbFileNames.getSelectedIndex();
+				long filesize = f.length();
+				String filename;/* = f.getName();*/
+				filename = Window.cbFileNames.getSelectedItem().toString();
 				
-				for (int i = sheetIndex.get(sheet); i > 0; i--) {
-					newFile.add(is.read());
+				byte[] buf;
+				
+				long iscounter = 0;
+				InputStream is = null;
+				OutputStream os = null;
+				
+				try {
+					is = new FileInputStream(gfxFileLocation);
+					os = new FileOutputStream(newFile);
+					
+					//Copy the first header
+					buf = new byte[256];
+					is.read(buf);
+					iscounter += 256;
+					os.write(buf, 0, 256);
+					
+					//Get number of sheets
+					numOfSheets = buf[8] & 0xff + ((buf[9] & 0xff) * 256);
+					//System.out.println(numOfSheets);
+					byteTracker = 256 + (268 * numOfSheets);
+					
+					//Directly copy the preceding sheet headers
+					for (int i = 0; i < sheet; i++) {
+						buf = new byte[268];
+						iscounter += 268;
+						is.read(buf);
+						os.write(buf, 0 ,268);
+	
+						byteTracker += sheetSize.get(i);
+						System.out.println(byteTracker);
+					}
+					
+					//Write the new sheet's header
+					writeLittleEndian(os, byteTracker, 4);
+					writeLittleEndian(os, filesize, 4);
+					writeLittleEndian(os, filesize, 4);
+					
+					for (int i = 0; i < 256; i++) {
+						if (i < filename.length()) {
+							os.write(filename.charAt(i));
+						}else{
+							os.write(0);
+						}
+					}
+					
+					is.skip(268);
+					iscounter += 268;
+					byteTracker += filesize;
+					
+					//Finish writing the old headers
+					for (int i = sheet + 1; i < numOfSheets; i++) {
+						writeLittleEndian(os, byteTracker, 4);
+						is.skip(4);
+						iscounter += 4;
+						
+						buf = new byte[264];
+						iscounter += 264;
+						is.read(buf);
+						os.write(buf, 0, 264);
+	
+						byteTracker += sheetSize.get(i);
+					}
+					
+					//Write sheets
+					for (int i = 0; i < numOfSheets; i++) {
+						int bufferSize;
+						InputStream newis = new FileInputStream(f);
+						if (i == sheet) {
+							
+							bufferSize = (int)f.length();
+							buf = new byte[bufferSize];
+							newis.read(buf);
+							
+						}else{
+							bufferSize = sheetSize.get(i);
+							buf = new byte[bufferSize];
+							is.skip(sheetIndex.get(i) - iscounter);
+							iscounter += sheetIndex.get(i) - iscounter;
+							iscounter += bufferSize;
+							is.read(buf);
+						}
+						os.write(buf, 0, bufferSize);
+						//System.out.println(bufferSize);
+						newis.close();
+					}
+					
+					is.close();
+					os.close();
+					
+					//Delete old file, and replace with the new one
+					File old = new File(gfxFileLocation);
+					old.delete();
+					newFile.renameTo(old);
+										
+					//Reset the combo box selection
+					int si = Window.cbFileNames.getSelectedIndex();
+					loadGfxFile();
+					Window.cbFileNames.setSelectedIndex(si);
+					updatePreview(Window.cbFileNames.getSelectedIndex());
+					
+					Window.showMessage("File import complete successfully!");
+					
+				}catch(Exception e){
+					System.out.println(e.toString());
+				}finally{
+					
 				}
-				is.close();
-				
-				//Import new file
-				is = new FileInputStream(chosenFile);
-				int read = is.read();
-				while (read != -1) {
-					newFile.add(read);
-					read = is.read();
-				}
-				is.close();
-				
-				//Finish importing old file
-				is = new FileInputStream(gfxFileLocation);
-				is.skip(sheetIndex.get(sheet + 1));
-				read = is.read();
-				while (read != -1) {
-					newFile.add(read);
-					read = is.read();
-				}
-				is.close();
-				
-				//Output new file
-				os = new FileOutputStream(gfxFileLocation);
-				for (int i : newFile) {
-					os.write(i);
-				}
-				os.close();
-				
-				updatePreview(sheet);
-				Window.showMessage("File Import Complete.");
-				
-			}catch(Exception e){
-			}finally{
-				
+			}else{
+				Window.showMessage("The file selected was not a BMP file. Please only import 8-bit BMP files.");
 			}
-			
-			showButtons(true);
+		}else{
+			newFile.delete();
 		}
-		
+				
+		showButtons(true);		
 	}
 	
 	public static void updatePreview(int sheetind) {
 		InputStream is = null;
 		OutputStream os = null;
-		ArrayList<Integer> previewBuffer = new ArrayList<Integer>();
-		boolean end = false;
-		int byte1 = 0, byte2 = 0;
-		int read = 0;
+		
 		try {
 			is = new FileInputStream(gfxFileLocation);
 			os = new FileOutputStream(new File("preview.bmp"));
 			is.skip(sheetIndex.get(sheetind));
 			
-			read = is.read();
-			previewBuffer.add(read);
-			read = is.read();
+			byte[] buf = new byte[sheetSize.get(sheetind)];
 			
-			while (end == false && read != -1) {
-				
-				previewBuffer.add(read);
-				
-				if (byte1 == (int)'B' && byte2 == (int)'M' && read != (int)'P' && read != (int)'p') {
-					//Found a sheet
-					end = true;
-					previewBuffer.remove(previewBuffer.size() - 1);
-					previewBuffer.remove(previewBuffer.size() - 1);
-					previewBuffer.remove(previewBuffer.size() - 1);
-				}
-				
-				byte1 = byte2;
-				byte2 = read;				
-				read = is.read();
-			}
-			
-			//Write file
-			for (int i : previewBuffer) {
-				os.write(i);
-			}
-			
-		}catch(Exception e){			
+			is.read(buf);
+			os.write(buf, 0, sheetSize.get(sheetind));
+			BufferedImage bi = ImageIO.read(new File("preview.bmp"));
+			Window.canPreview.setSize(new Dimension(bi.getWidth(), bi.getHeight()));
+			//Window.scrollPane.setSize(new Dimension(bi.getWidth(), bi.getHeight()));
+		}catch(Exception e){
+			System.out.println(e.toString());
 		}finally{
 			if (is != null) {
 				try {
@@ -299,76 +397,6 @@ public class Main {
 				} catch (IOException e) { e.printStackTrace(); }
 			}
 		}
-	}
-	
-	private static boolean populateFileNames() {
-		boolean result = true;
-		
-		Window.clearFileNames();
-		InputStream is = null;
-		int thisByte;
-		boolean end = false;
-		int buffer[] = new int[2000];
-		int buffindex = -1;
-		
-		try {			
-			is = new FileInputStream(gfxFileLocation);
-			
-			while (end == false) {
-				buffindex += 1;
-				
-				thisByte = is.read();
-				buffer[buffindex] = thisByte;
-				
-				//Find '.bmp'
-				if (buffindex > 4) {
-					if ((thisByte == (int)'p' || thisByte == (int)'P') &&
-						(buffer[buffindex - 1] == (int)'m' || buffer[buffindex - 1] == (int)'M') &&
-						(buffer[buffindex - 2] == (int)'b' || buffer[buffindex - 2] == (int)'B') &&
-						(buffer[buffindex - 3] == (int)'.'))
-					{
-						String filenameback = "";
-						for (int i = 0; i < buffindex; i++) {
-							if (buffer[buffindex - i] == 0) { //End stepback
-								i = buffindex;
-								buffindex = 0;
-							}else{
-								filenameback += (char)buffer[buffindex - i];
-							}
-						}
-						String filename = "";
-						for (int i = filenameback.length() - 1; i >= 0; i--) {
-							filename += filenameback.charAt(i);
-						}
-						Window.addFileName(filename);
-					}
-				}
-				
-				//Find 'BM' but not 'BMP'
-				if (buffindex > 3) {
-					if (thisByte != (int)'P' && thisByte != (int)'p' && buffer[buffindex - 1] == (int)'M' &&
-						buffer[buffindex - 2] == (int)'B')
-					{
-						end = true;
-					}
-				}
-			}				
-			
-		}catch(Exception e) {
-			e.printStackTrace();
-			Window.showMessage(e.getMessage());
-			result = false;
-		}finally {
-			
-			if (is!= null) {
-				try {
-					is.close();
-				} catch (IOException e) {e.printStackTrace();}
-			}
-			
-		}
-		
-		return result;
 		
 	}
 	
@@ -378,32 +406,54 @@ public class Main {
 			try {
 				BufferedReader br = new BufferedReader(new FileReader("config.txt"));
 				gfxFileLocation = br.readLine();
-				lastSave = br.readLine();
+				lastExport = br.readLine();
+				lastImport = br.readLine();
 				br.close();
 			}catch(Exception e){};
 		}
-		if (lastSave.equals("")) {
-			lastSave = localPath;
+		if (lastExport.equals("")) {
+			lastExport = localPath;
 		}
-		if (gfxFileLocation.equals("")) {
-			//gfxFileLocation = localPath;
+		if (lastImport.equals("")) {
+			lastImport = localPath;
 		}
+		
+		File gfx = new File(gfxFileLocation);
+		if (!gfx.exists()) {
+			gfxFileLocation = "";
+		}
+		
+		/*
+		System.out.println(gfxFileLocation);
+		System.out.println(lastImport);
+		System.out.println(lastExport);
+		*/
 	}
 	
 	private static void saveConfig() {
-		if (lastSave.equals("")) {
-			lastSave = localPath;
+		if (lastExport.equals("")) {
+			lastExport = localPath;
+		}
+		if (lastImport.equals("")) {
+			lastImport = localPath;
 		}
 		if (gfxFileLocation.equals("")) {
-			gfxFileLocation = localPath;
+			gfxFileLocation = "";
 		}
 		try {
 			OutputStream os = new FileOutputStream(new File("config.txt"));
 			PrintStream ps = new PrintStream(os);
 			ps.println(gfxFileLocation);
-			ps.println(lastSave);
+			ps.println(lastExport);
+			ps.println(lastImport);
 			ps.close();
 		}catch(Exception e){};
+		
+		/*
+		System.out.println(gfxFileLocation);
+		System.out.println(lastImport);
+		System.out.println(lastExport);
+		*/
 	}
 
 }
