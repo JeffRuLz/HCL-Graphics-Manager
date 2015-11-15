47bd90ae2028bf52ae3214ba274db86c24205370
JeffRuLz <yomanrulz@hotmail.com>
2015-11-14 22:11:59 -0600

    Update Window.java

--- src/Window.java
+++ src/Window.java
@@ -3,51 +3,56 @@ import javax.swing.JMenuBar;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.awt.Component;
 
 public class Window extends JFrame implements ActionListener, ItemListener {
 
 	public static JFrame frame;
 	public static JComboBox cbFileNames;
-	public static Preview canPreview;
 	public static JButton btnExport;
 	public static JButton btnImport;
+	public static Preview canPreview;
 
 	public Window() {
 		initialize();
 	}
 
 	private void initialize() {
+		//JFrame.setDefaultLookAndFeelDecorated(true);
 		frame = new JFrame();
-		frame.setBounds(100, 100, 588, 473);
+		frame.getContentPane().setLayout(null);
+		frame.setPreferredSize(new Dimension(600, 480));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Hydra Castle Graphics Manager v1.1");
+		frame.setTitle("Hydra Castle Graphics Manager v1.4");
 		
+	//Menubar
 		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
-								
-		JMenu mnFile = new JMenu("File");
-		menuBar.add(mnFile);
-				
+		menuBar.setBounds(0, 0, 594, 20);
+		
+		JMenu mnFile = new JMenu("File");	
+		
 		JMenuItem mntmOpenBmpqda = new JMenuItem("Open Bmp.qda...");
 		mntmOpenBmpqda.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent arg0) {
 				try {
 					Main.openQdaFile();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				} catch (IOException e) {e.printStackTrace();}
 			}
 		});
 		mnFile.add(mntmOpenBmpqda);
@@ -59,10 +64,19 @@ public class Window extends JFrame implements ActionListener, ItemListener {
 				System.exit(0);
 			}
 		});
+		
 		mnFile.add(mntmQuit);
-		frame.getContentPane().setLayout(null);
+		menuBar.add(mnFile);	
+		frame.getContentPane().add(menuBar);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 31, 145, 407);
+		frame.getContentPane().add(panel);
 		
 		cbFileNames = new JComboBox();
+		cbFileNames.setLocation(0, 0);
+		cbFileNames.setAlignmentX(Component.LEFT_ALIGNMENT);
+		cbFileNames.setSize(new Dimension(145, 20));
 		cbFileNames.setEnabled(false);
 		cbFileNames.addItemListener(new ItemListener() {
 			public void itemStateChanged(ItemEvent arg0) {
@@ -71,13 +85,14 @@ public class Window extends JFrame implements ActionListener, ItemListener {
 				Main.updatePreview(cbFileNames.getSelectedIndex());
 			}
 		});
-		cbFileNames.setBounds(416, 11, 146, 20);
-		frame.getContentPane().add(cbFileNames);
+		panel.setLayout(null);
+		panel.add(cbFileNames);
 		
 		btnExport = new JButton("Export...");
+		btnExport.setBounds(0, 31, 95, 23);
 		btnExport.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent arg0) {
+			public void mousePressed(MouseEvent arg0) {
 				
 				if (btnExport.isEnabled() == true) {
 					Main.exportImage();
@@ -85,35 +100,35 @@ public class Window extends JFrame implements ActionListener, ItemListener {
 				
 			}
 		});
-		btnExport.setBounds(416, 42, 146, 23);
-		frame.getContentPane().add(btnExport);
+		panel.add(btnExport);
 		
 		btnImport = new JButton("Import...");
+		btnImport.setBounds(0, 65, 95, 23);
 		btnImport.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent arg0) {
+			public void mousePressed(MouseEvent arg0) {
 				
 				if (btnImport.isEnabled() == true) {
 					try {
 						Main.importImage();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+					} catch (IOException e) {e.printStackTrace();}
 				}
 				
 			}
 		});
-		btnImport.setBounds(416, 76, 146, 23);
-		frame.getContentPane().add(btnImport);
+		panel.add(btnImport);
 		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(165, 32, 417, 406);
+		frame.getContentPane().add(panel_1);
 		
 		canPreview = new Preview();
-		canPreview.setBackground(Color.WHITE);
-		canPreview.setBounds(10, 11, 400, 400);
-		frame.getContentPane().add(canPreview);
+		panel_1.add(canPreview);
+		canPreview.setBackground(Color.WHITE);		
 		
+		frame.pack();
 		frame.setVisible(true);
+		frame.setResizable(false);
 	}
 	
 	public static void showMessage(String mess) {
@@ -137,5 +152,4 @@ public class Window extends JFrame implements ActionListener, ItemListener {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {}
-	
 }
